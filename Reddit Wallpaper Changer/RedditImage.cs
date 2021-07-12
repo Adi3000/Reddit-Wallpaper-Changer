f42901241f390649421f345c6c5a87a1963a3a57
qwertydog <campbelldeas@hotmail.com>
2021-07-13 10:41:11 +1200

    Disambiguate names

--- Reddit Wallpaper Changer/RedditImage.cs
+++ Reddit Wallpaper Changer/RedditImage.cs
@@ -1,20 +1,21 @@
 ﻿using Reddit_Wallpaper_Changer.Properties;
 using System;
+using System.Drawing;
 using System.IO;
 
 namespace Reddit_Wallpaper_Changer
 {
     public class RedditImage
     {
-        public string Image { get; }
+        public string Thumbnail { get; }
         public string Title { get; }
         public string ThreadId { get; }
         public string Url { get; }
         public string Date { get; }
 
-        public RedditImage(string image, string title, string threadId, string url, string date)
+        public RedditImage(string thumbnail, string title, string threadId, string url, string date)
         {
-            Image = image;
+            Thumbnail = thumbnail;
             Title = title;
             ThreadId = threadId;
             Url = url;
@@ -27,9 +28,9 @@ namespace Reddit_Wallpaper_Changer
 
             if (File.Exists(fileName)) return;
 
-            var ms = new MemoryStream(Convert.FromBase64String(Image));
+            var ms = new MemoryStream(Convert.FromBase64String(Thumbnail));
 
-            using (var image = System.Drawing.Image.FromStream(ms))
+            using (var image = Image.FromStream(ms))
             {
                 image.Save(fileName);
             }
