afef14df39fab3e3acd38ee9c8b94a0a693ce74e
qwertydog <campbelldeas@hotmail.com>
2022-01-31 14:51:49 +1300

    Bump version

--- Reddit Wallpaper Changer/Properties/AssemblyInfo.cs
+++ Reddit Wallpaper Changer/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("/u/qwertydog")]
 [assembly: AssemblyProduct("Reddit Wallpaper Changer")]
-[assembly: AssemblyCopyright("Copyright © /u/qwertydog 2016-2021")]
+[assembly: AssemblyCopyright("Copyright © /u/qwertydog 2016-2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.17.0")]
-[assembly: AssemblyFileVersion("1.0.17.0")]
+[assembly: AssemblyVersion("1.0.18.0")]
+[assembly: AssemblyFileVersion("1.0.18.0")]
 
